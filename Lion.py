52d67083a5014790e51fe8a74b15c0118991b810
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-05-14 15:47:52 +0530

    Update Lion.py

--- Lion.py
+++ Lion.py
@@ -6,7 +6,7 @@ logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
 )
 # Starting bot
-system("git clone https://github.com/desinobita/SuzuneHorikita && cd SuzuneHorikita && pip3 install requirements.txt && python3 -m SuzuneHorikita")
+system("git clone https://github.com/desinobita/SuzuneHorikita && cd SuzuneHorikita && python3 -m SuzuneHorikita")
 # if got error
 print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ")
 # Script written for stop heroku suspension
